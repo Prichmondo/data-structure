8df3957a24b8f3a6426f6323b4ffd350640c3363
ricchie <riccardo.volpato@ding.com>
2022-07-24 22:48:42 +0100

    test: removed js compiled file

--- src/classes/stack.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const stack_1 = require("./stack");
-it('is empty when created without arguments', () => {
-    const stack = new stack_1.Stack();
-    expect(stack.length()).toBe(0);
-});
